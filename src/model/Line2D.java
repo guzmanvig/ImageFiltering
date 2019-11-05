eecce6dd3baf607f9c393060a00df0b3273d63fb
Guz <guzmanvigliecca@gmail.com>
2019-11-04 19:51:54 -0800

    Added comments

--- src/model/Line2D.java
+++ src/model/Line2D.java
@@ -1,19 +1,35 @@
 package model;
 
+/**
+ * Class that represents a line with a start point and an end point in a 2D plane.
+ */
 public class Line2D {
 
   private final Point2D start;
   private final Point2D end;
 
+  /**
+   * Cretes a line.
+   * @param start the point where the line starts.
+   * @param end the point where the line ends.
+   */
   public Line2D(Point2D start, Point2D end) {
     this.start = start;
     this.end = end;
   }
 
+  /**
+   * Get the start point of the line.
+   * @return the start point.
+   */
   public Point2D getStart() {
     return start;
   }
 
+  /**
+   * Get the end point of the line.
+   * @return the end point.
+   */
   public Point2D getEnd() {
     return end;
   }
